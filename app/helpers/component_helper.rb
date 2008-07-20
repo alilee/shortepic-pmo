3c2c9389c156ba56b80e2d25f13dc560f8589b86
Alister Lee <pmo@dev.shortepic.com>
2008-07-20 17:06:33 +1000

    Display product break-down structure in show view

---
+++ app/helpers/component_helper.rb
@@ -0,0 +1,19 @@
+module ComponentHelper
+
+  # TODO: C - optimise to do one query and use classify to process
+  def component_descendants_tree(component_detail)
+    descendants = component_detail.children.select { |c| c.component.status.incomplete? }
+  
+    return '' if descendants.empty?
+    
+    result = "<ul>"
+    descendants.each do |c_d|
+      result << "<li>#{link_to_item c_d.component}</li>"
+      result << component_descendants_tree(c_d)
+    end
+    result << "</ul>"
+
+    result
+  end
+
+end
